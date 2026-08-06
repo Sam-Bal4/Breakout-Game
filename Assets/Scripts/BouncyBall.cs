1e0531c2886937a7fe0f4b30080d1f18b8c168dc
Samuel T Balfour <samuelbalfour34@gmail.com>
2026-08-06 17:20:15 -0600

    Ball velocity is now limited

--- Assets/Scripts/BouncyBall.cs
+++ Assets/Scripts/BouncyBall.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class BouncyBall : MonoBehaviour
 {
     public float minY = -5.5f;
+    public float maxVelocity = 20f;
+
+    Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,6 +18,11 @@ public class BouncyBall : MonoBehaviour
     {
         if(transform.position.y < minY){
             transform.position = Vector3.zero;
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        if(rb.linearVelocity.magnitude > maxVelocity){
+            rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
         }
     }
 }
